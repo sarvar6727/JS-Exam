c5ce8fc7288fda5a0e6831108ce1af1e470cd0b2
sarvar6727 <sarvarsultonoff@gmail.com>
2022-12-01 17:11:53 +0500

    end

--- js/app.js
+++ js/app.js
@@ -1,2 +1,60 @@
-// Savollar
-// 1) 
+/*Savollar
+1) 2 xil boladi o'zgaruvchan va o'zgarmas
+    elon qilish 3 xil kalit so'z bilan 
+    let, var, const
+    let va var kalit so'zlari bilan ochilgan o'zgaruvchini qiymatini o'zgartirish mumkin
+    const kalit so'zi bilan ochilgan ozgaruvchini qiymatini o'zgartirib bo'lmaydi
+
+
+2) number, string, boolean, null, undefined
+
+3) Increment ++ operatori 
+    a = a + 1 ning qisqartmasi a++ (a+=1)
+    Decrement -- operatori
+    a = a - 1 nign qisqartmasi a-- (a-=1)
+
+4) string + number = string
+
+5) && va || mantiqiy opratorlar 
+    && va 
+    || yoki
+    boolean qiymat qaytaradi
+
+    true && false = false
+    true && true = true
+    false && false = false
+    true || true = true
+    true || false = true
+    false || false = false
+
+6)if else shart operatori
+    if(bool){
+        //code
+    }else{  
+        //code
+    }
+
+    bool true bolsa if qism ishlaydi agar false bolsa else qismi ishlaydi
+
+7) if(shart){
+        //code
+    }
+    else if(shart){
+        //code
+    }
+    else{
+        //code
+    }
+
+8) massiv malumotlarni index boyicha saqlaydi 
+    massiv[massiv.length - 1] // oxirgi elementni olish
+
+9) obyekt malumotlarni kalit va qiymat ko'rinishida saqlaydi 
+    let obj = {
+        "key": value
+    }
+
+10) method bu obyektning funksiyasi
+
+new String().toLowerCase() method
+*/ 
